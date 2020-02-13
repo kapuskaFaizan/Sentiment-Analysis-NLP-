a8c3d01e90f0ac7e238e66b110d0d09e88822c4d
kapuskaFaizan <44909493+kapuskaFaizan@users.noreply.github.com>
2020-02-13 14:38:13 +0530

    Update senti_decisiontre.py

--- senti_decisiontre.py
+++ senti_decisiontre.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 from sklearn import tree
 from sklearn.datasets import load_iris
@@ -11,9 +9,8 @@ import numpy as np
 import re
 import pandas as pd
 
-csv = pd.read_csv('C:/Users/faiza/OneDrive/Desktop/data_sets/tweets.csv')
-
-df = pd.DataFrame(csv)
+location = sys.argv[0]
+df = pd.read_csv(location)
 
 df1=df[['text','airline_sentiment']]
 
@@ -31,15 +28,14 @@ df1['airline_sentiment'].replace('positive', int('1'), regex =True, inplace = Tr
 df1['airline_sentiment'].replace('negative', int('-1'), regex =True, inplace = True)
 
 from nltk.tokenize import word_tokenize
-tokenized_tweet = df1['text'].apply(lambda x: x.split() )
-
-
 from nltk.stem.porter import *
+
+tokenized_tweet = df1['text'].apply(lambda x: x.split() )
 stemmer = PorterStemmer()
 
 tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) # stemming
 tokenized_tweet.head()
-.
+
 for i in range(len(tokenized_tweet)):
     tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
 
@@ -60,11 +56,6 @@ test_bow = bow[31962:,:]
 
 x_train, x_test, y_train, y_test = train_test_split(train_bow, df1['airline_sentiment'], random_state=42, test_size=0.3)
 
-print( bow)
-
-
-# In[2]:
-
 
 classifr = tree.DecisionTreeClassifier()
 classifr.fit(x_train,y_train)
@@ -72,14 +63,8 @@ acc_train = np.sum(classifr.predict(x_train) == y_train)/ float(y_train.size)
 acc_test = np.sum(classifr.predict(x_test) == y_test)/ float(y_test.size)
 
 
-# In[3]:
-
-
 print('classification accuracy for train',acc_train)
 print('classification accuracy for test',acc_test)
-
-
-# In[4]:
 
 
 y_pred=classifr.predict(x_test)
